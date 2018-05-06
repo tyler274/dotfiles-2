45790c57e7287c29310cf40dbd333f0ff388a35b
Tyler Port <tyler274port@gmail.com>
2018-05-06 15:51:47 -0700

    remove pure prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,6 @@
 #!/bin/zsh
-PURE_CMD_MAX_EXEC_TIME=1
-PURE_PROMPT_SYMBOL="λ"
+#nerd fonts
+export POWERLEVEL9K_MODE='nerdfont-complete'
+export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir rbenv nvm vcs)
+export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
+
