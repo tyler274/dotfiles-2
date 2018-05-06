5001b18e35a39c473f983fe796b29b17865be004
Tyler Port <tyler274port@gmail.com>
2018-05-06 15:10:52 -0700

    updates

---
+++ node/completion.zsh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
--- node/path.zsh
+++ node/path.zsh
@@ -1,2 +1,4 @@
 #!/bin/sh
 export PATH="$PATH:$HOME/.npm/bin:$HOME/.bin"
+export NVM_LAZY_LOAD=true
+export NVM_AUTO_USE=true
