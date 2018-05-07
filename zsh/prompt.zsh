e08244bfd49633fcbd6ad5911e1d092e9773a74a
tyler274 <tyler274port@gmail.com>
2018-05-07 14:39:36 -0700

    switch to p9k

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 #nerd fonts
-export POWERLEVEL9K_MODE='nerdfont-complete'
-export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir rbenv nvm vcs)
-export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
-export POWERLEVEL9K_GENERATOR="zsh-async"
+export P9K_MODE='nerdfont-complete'
+export P9K_LEFT_PROMPT_ELEMENTS=(context dir rbenv nvm vcs)
+export P9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
+export P9K_GENERATOR="zsh-async"
